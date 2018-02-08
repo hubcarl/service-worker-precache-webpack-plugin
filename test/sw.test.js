5634216a8842135db25eed1de8a2ef0204dfe224
caoli <cl98914@alibaba-inc.com>
2018-02-08 18:08:41 +0800

    Release 1.0.3

---
+++ test/sw.test.js
@@ -0,0 +1,24 @@
+'use strict';
+const expect = require('chai').expect;
+const path = require('path');
+const fs = require('fs');
+// http://chaijs.com/api/bdd/
+describe('sw.test.js', () => {
+  before(() => {
+  });
+
+  after(() => {
+  });
+
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+  });
+
+  describe('#webpack sw test', () => {
+    it('should config html template', () => {
+     
+    });
+  });
+});
